679eadf5b4598db1106340d481787d1e028b3b29
wangjiayan <wangjiayan@shoplineapp.com>
2022-06-20 23:34:54 +0800

    feat:去掉lock文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
+  title: '你好， VuePress111 ！',
+  description: '这是我的第一个 VuePress 站点1',
   base: '/', // 部署站点的基础路径
   // 导航栏配置，最深两级嵌套
   navbar: [
@@ -12,7 +12,12 @@ module.exports = {
   {
     text:'贡献',
     link:'/contributing/'
-  },
-
-   ]
+  },],
+  sidebar:{
+    '/guild/':[
+      {
+        children: ['/guild/README.md'],
+      }
+    ]
+  }
 }
